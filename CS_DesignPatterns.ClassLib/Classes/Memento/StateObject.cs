636d0a8c7781287f07710b44ee2f3ec7c376e72f
Marco Preciado <mac46005@gmail.com>
2021-10-25 13:10:46 -0500

    Memento Design Pattern: StateObject

---
+++ CS_DesignPatterns.ClassLib/Classes/Memento/StateObject.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CS_DesignPatterns.ClassLib.Classes.Memento
+{
+    //* This is the object to keep state in memento
+    //* It needs to implement ICloneable interface to provide a deep copy object,
+    //* Otherwise the memento could be filled with shallow copies with references to the same object in memory
+    public class StateObject : ICloneable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public object Clone()
+        {
+            return new StateObject{
+                Id = this.Id,
+                Name = this.Name
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id:{this.Id}, Name:{this.Name}";
+        }
+    }
+}
