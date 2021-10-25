4f4d5ceeb89e6d5cc8c280276a4741d57a6e3fed
Marco Preciado <mac46005@gmail.com>
2021-10-24 19:41:06 -0500

    Singleton Thread Unsafe

--- CS_DesignPatterns.ClassLib/Classes/SingletonClassNotThreadSafe.cs
+++ CS_DesignPatterns.ClassLib/Classes/SingletonClassNotThreadSafe.cs
@@ -1,17 +1,24 @@
-//Not Thread-safe version of a Singleton
-public sealed class SingletonClass
+
+namespace CS_DesignPatterns.ClassLib.Classes
 {
-    private static SingletonClass _instance = null;
-    private SingletonClass(){}
-    public static SingletonClass SingleInstance{
-        get{
-            if(_instance == null){
-                // Is is possible that two different threads reach this point at the same time,
-                // so more than one instance can be created in a situation like this, because both will
-                // pass the condition about _instance being null
-                _instance = new SingletonClass();
+    //Not Thread-safe version of a Singleton
+    public sealed class SingletonClassNotThreadSafe
+    {
+        private static SingletonClassNotThreadSafe _instance = null;
+        private SingletonClassNotThreadSafe() { }
+        public static SingletonClassNotThreadSafe SingleInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    // Is is possible that two different threads reach this point at the same time,
+                    // so more than one instance can be created in a situation like this, because both will
+                    // pass the condition about _instance being null
+                    _instance = new SingletonClassNotThreadSafe();
+                }
+                return _instance;
             }
-            return _instance;
         }
     }
 }
