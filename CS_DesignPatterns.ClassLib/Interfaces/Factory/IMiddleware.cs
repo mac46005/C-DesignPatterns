ae30f0ec9052bc18d99261f3e4ad880bf052b9fd
Marco Preciado <mac46005@gmail.com>
2021-10-24 20:25:53 -0500

    Factory Design Pattern: IMiddleware

--- CS_DesignPatterns.ClassLib/Interfaces/Factory/IMiddleware.cs
+++ CS_DesignPatterns.ClassLib/Interfaces/Factory/IMiddleware.cs
@@ -1,4 +1,9 @@
 namespace CS_DesignPatterns.ClassLib.Interfaces.Factory{
+
+    //* Factory Design Pattern
+    //? This is a creational pattern that uses a specialized object to create other objects, abstracting the use of concrete objects. This is a pattern extensivelly used in strategies to reduce coupling in the code.
+
+    //* A simple implementation of a Factory Pattern using an interface
     public interface IMiddleware{
         void DoAction();
     }
