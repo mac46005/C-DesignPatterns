62c941b74f83c5d6344d75c9fa8b43c71a842894
Marco Preciado <mac46005@gmail.com>
2021-10-24 20:29:43 -0500

    Factory Design Pattern: Middleware1

--- CS_DesignPatterns.ClassLib/Classes/Factory/Middleware1.cs
+++ CS_DesignPatterns.ClassLib/Classes/Factory/Middleware1.cs
@@ -1,9 +1,13 @@
 using System;
 using CS_DesignPatterns.ClassLib.Interfaces.Factory;
 
+//* Factory Design Pattern
+//? This is a creational pattern that uses a specialized object to create other objects, abstracting the use of concrete objects. This is a pattern extensivelly used in strategies to reduce coupling in the code.
+//* The concrete class with specific implementations for the interface method
 namespace CS_DesignPatterns.ClassLib.Classes.Factory{
     public class Middleware1 : IMiddleware
     {
+        //* Implementing the interface method
         public void DoAction()
         {
             Console.WriteLine("Executing action from Middleware1");
