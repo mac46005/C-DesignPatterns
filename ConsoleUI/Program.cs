3a2a1d744293fdb4687e5c751c5611a4f6131028
Marco Preciado <mac46005@gmail.com>
2021-10-24 19:57:48 -0500

    Added new ClassLib

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CS_DesignPatterns.ClassLib.Classes;
 
 namespace ConsoleUI
 {
@@ -6,6 +7,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
